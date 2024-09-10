6519c2a2322f64eb26b3f4b9e213e91437588bb4
Soojin Kim <contact.soojin.kim@gmail.com>
2024-09-10 10:12:51 +0200

    added address column to restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_10_080538) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_10_081150) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
